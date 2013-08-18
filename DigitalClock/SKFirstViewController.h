121d44369b78f586d2f912b33132f16cd48b48d6
Aleksand Skorohodov <skoroxod@mail.ru>
2013-08-19 01:33:16 +0600

    add clock to first tab

--- DigitalClock/SKFirstViewController.h
+++ DigitalClock/SKFirstViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface SKFirstViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *clockText;
 
 @end
--- DigitalClock/SKFirstViewController.m
+++ DigitalClock/SKFirstViewController.m
@@ -9,7 +9,11 @@
 #import "SKFirstViewController.h"
 
 @interface SKFirstViewController ()
+{
+    NSTimer *myTicker;
 
+    
+}
 @end
 
 @implementation SKFirstViewController
@@ -18,12 +22,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self runTimer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)runTimer {
+
+    myTicker = [NSTimer scheduledTimerWithTimeInterval: 0.5
+                                                target: self
+                                              selector: @selector(refreshTime)
+                                              userInfo: nil
+                                               repeats: YES];
+    
+}
+
+- (void)refreshTime {
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSDate *date = [NSDate date];
+    
+    [formatter setTimeStyle:NSDateFormatterFullStyle];
+    
+    [_clockText setText:[formatter stringFromDate:date]];
+    
 }
 
 @end
