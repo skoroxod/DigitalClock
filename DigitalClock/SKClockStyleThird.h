525704b71f5f08c4934c154b5773814aa1999d87
Aleksand Skorohodov <skoroxod@mail.ru>
2013-08-21 01:55:03 +0600

    Digital clock first release

---
+++ DigitalClock/SKClockStyleThird.h
@@ -0,0 +1,13 @@
+//
+//  SKClockStyleThird.h
+//  DigitalClock
+//
+//  Created by Aleksandr Skorokhodov on 21.08.13.
+//  Copyright (c) 2013 Aleksandr Skorokhodov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKClockStyleThird : NSObject
+
+@end
